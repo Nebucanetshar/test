28579f63de1c930ea54935a61873292707e8a02b
nebucanetshar <alhannb@hotmail.com>
2024-07-03 16:59:02 +0200

    server works

--- grpc/Data/AppDbContext.cs
+++ grpc/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using grpc.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace grpc.Data;
 
--- grpc/obj/Debug/net8.0/grpc.AssemblyInfo.cs
+++ grpc/obj/Debug/net8.0/grpc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("grpc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dee2b1726dec02ddba959dfe4c938fa538b79b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba351df7f76bcb579fb463a1a2c19e6c5da1c5b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("grpc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("grpc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
