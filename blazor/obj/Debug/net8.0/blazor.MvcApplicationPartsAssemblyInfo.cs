b0ec1630fd482c47f0231f6b9e852067cbf5c477
nebucanetshar <alhannb@hotmail.com>
2024-07-12 13:43:34 +0200

    blazor and grpc debug works

--- blazor/obj/Debug/net8.0/blazor.MvcApplicationPartsAssemblyInfo.cs
+++ blazor/obj/Debug/net8.0/blazor.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("blazor")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438c7b6d0a9b717245161e0183085054da6a9c94")]
-[assembly: System.Reflection.AssemblyProductAttribute("blazor")]
-[assembly: System.Reflection.AssemblyTitleAttribute("blazor")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("grpc")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Grpc.JsonTranscoding")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
--- grpc/obj/Debug/net8.0/grpc.AssemblyInfo.cs
+++ grpc/obj/Debug/net8.0/grpc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("grpc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4de55395abfb377c0fe08a6bb8b8b47ab9c1671e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438c7b6d0a9b717245161e0183085054da6a9c94")]
 [assembly: System.Reflection.AssemblyProductAttribute("grpc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("grpc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
