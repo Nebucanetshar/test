9a4ae602f44026eb18a323a2dc538b17012258f8
nebucanetshar <alhannb@hotmail.com>
2024-07-16 12:32:06 +0200

    genereted folder GreetClient.cs and GreetClientGrpc.cs don't forget add package neccessary with add greetClient.proto in blazor.csproj

--- app/obj/Debug/net8.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("grpc")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("grpc")]
-[assembly: System.Reflection.AssemblyTitleAttribute("grpc")]
+[assembly: System.Reflection.AssemblyProductAttribute("app")]
+[assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
