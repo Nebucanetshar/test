051de404cc44a5c1b2c31014e619ad4be42fc83d
nebucanetshar <alhannb@hotmail.com>
2024-07-26 11:04:04 +0200

    add file Grpc.Razor.cs

--- app/obj/Debug/net8.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4b751e1315f1865c76f6eab93cc4088a13cfbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
