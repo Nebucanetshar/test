aacafd10dbde45aad83b90907c2dc8e40386f526
nebucanetshar <alhannb@hotmail.com>
2024-08-05 20:42:58 +0200

    add binding Person

---
+++ Models/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace wpf.Models;
+
+public class Person
+{
+    public string ?Nom {  get; set; }
+    public string ?Prenom { get; set; }
+}
--- obj/Debug/net8.0-windows/wpf.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/wpf.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("wpf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c275efc099b357f78e2a88faaab3295a95e470f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("wpf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wpf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
