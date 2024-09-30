26d87462bc715f9580701e061dedffd95296c378
nebucanetshar <alhannb@hotmail.com>
2024-09-30 11:01:00 +0200

    ??

--- grpc/obj/Debug/net8.0/grpc.AssemblyInfo.cs
+++ grpc/obj/Debug/net8.0/grpc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("grpc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051de404cc44a5c1b2c31014e619ad4be42fc83d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("grpc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("grpc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
