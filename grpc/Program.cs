7eaddfbe44eb6dc55eb9e00f0e7d746797d6a7f7
nebucanetshar <alhannb@hotmail.com>
2024-10-02 12:46:32 +0200

    transcoding server grpc works

--- grpc/Program.cs
+++ grpc/Program.cs
@@ -1,14 +1,44 @@
 using grpc.Services;
+using Microsoft.AspNetCore.Builder;
 
-var builder = WebApplication.CreateBuilder(args);
+public class GrpcProgram
+{
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddGrpc();
+		
+		builder.Services.AddGrpc().AddJsonTranscoding();
 
-var app = builder.Build();
+		var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-app.Run();
+		app.MapGrpcService<GreeterService>();
+		app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+		app.Run();
+	}
+
+	//************** Middleware **************
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.AddGrpc();
+		services.AddControllers();
+	}
+
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+	{
+		if (env.IsDevelopment())
+			app.UseDeveloperExceptionPage();
+
+		app.UseRouting();
+		app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
+
+		app.UseEndpoints(endpoints =>
+		{
+			endpoints.MapControllers();
+			endpoints.MapGrpcService<GreeterService>().EnableGrpcWeb();
+
+		});
+	}
+}
--- grpc/Services/GreeterService.cs
+++ grpc/Services/GreeterService.cs
@@ -1,21 +1,30 @@
 using Grpc.Core;
 using grpc;
+using app.Components.Pages;
 
 namespace grpc.Services;
 
-public class GreeterService : Greeter.GreeterBase
+public class GreeterService : Link.LinkBase
 {
-    private readonly ILogger<GreeterService> _logger;
-    public GreeterService(ILogger<GreeterService> logger)
+	private readonly ILogger<GreeterService>_logger;
+	public GreeterService(ILogger<GreeterService> logger)
+	{
+		_logger = logger;
+	}
+
+	public override async Task SayHelloStream(CounterRequest request, IServerStreamWriter<CounterResponse> responseStream, ServerCallContext context)
     {
-        _logger = logger;
+        if (request.Count <=0)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "le compteur doit $etre plus grand que Zeros"));
+        }
+        _logger.LogInformation($"l'utilisateur à appuyer {request.Count} fois");
+
+        for (var i=0; i<request.Count; i++)
+        {
+            await responseStream.WriteAsync(new CounterResponse { Message = $"tu a réussi a joindre les deux bout {i+1} fois " });
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
     }
 
-    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-    {
-        return Task.FromResult(new HelloReply
-        {
-            Message = "Hello " + request.Name
-        });
-    }
 }
