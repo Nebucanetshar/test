f335666b9dcf0058330194e2e47d0be2964cceca
nebucanetshar <alhannb@hotmail.com>
2024-10-27 20:26:59 +0100

    create file CounterServer.cs

---
+++ grpc/Services/CounterServer.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+
+namespace grpc;
+
+public class CounterServer:Counter.CounterBase
+{
+    public override async Task StartCounter(CounterRequest request, IServerStreamWriter<CounterResponse> response, ServerCallContext context)
+    {
+        var count = request.Start;
+
+        while (!context.CancellationToken.IsCancellationRequested)
+        {
+            await response.WriteAsync(new CounterResponse
+            {
+                Count = ++count
+            });
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
